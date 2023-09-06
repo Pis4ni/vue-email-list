195ffa32645bcc62106c51c36fbcef52a0ab070f
Pis4ni <pis4wrk@gmail.com>
2023-09-06 15:59:42 +0200

    add: milestone 0

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const { createApp } = Vue
+
+
+createApp({
+  data() {
+    return {
+      message:'deb-test',
+      mails:[]
+    }
+  },
+  methods:{
+    getMails(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=>{
+                const responseMail = response.data.response
+                this.mails.push(responseMail)
+                
+            })
+        }
+    },
+},
+created(){
+    console.log(axios);
+    this.getMails()
+    console.log(this.mails);
+    console.log(this.message);
+}
+}).mount('#app')
