999ce9e737916c64a2e56f6f7f8d79a47589f556
Pis4ni <pis4wrk@gmail.com>
2023-09-06 16:07:24 +0200

    add: EXTRA button

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ createApp({
   },
   methods:{
     getMails(){
+        this.mails = []
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response)=>{
